493b4bc73d1deaa70f297b66e60faefeddf3ced3
glennleo <glennleo@Les-MacBook-Pro.local>
2024-04-13 11:54:38 +0700

    update product card

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -1,5 +1,3 @@
-import { Text } from "@medusajs/ui"
-
 import { ProductPreviewType } from "types/global"
 
 import { retrievePricedProductById } from "@lib/data"
@@ -8,6 +6,7 @@ import { Region } from "@medusajs/medusa"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import Thumbnail from "../thumbnail"
 import PreviewPrice from "./price"
+import { Button } from "@medusajs/ui"
 
 export default async function ProductPreview({
   productPreview,
@@ -37,16 +36,27 @@ export default async function ProductPreview({
       href={`/products/${productPreview.handle}`}
       className="group"
     >
-      <div data-testid="product-wrapper">
-        <Thumbnail
-          thumbnail={productPreview.thumbnail}
-          size="full"
-          isFeatured={isFeatured}
-        />
-        <div className="flex txt-compact-medium mt-4 justify-between">
-          <Text className="text-ui-fg-subtle" data-testid="product-title">{productPreview.title}</Text>
-          <div className="flex items-center gap-x-2">
-            {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
+      <div data-testid="product-wrapper" className="bg-white rounded-lg overflow-hidden shadow-lg ring-opacity-40 max-w-sm">
+        <div className="relative">
+          <Thumbnail
+            thumbnail={productPreview.thumbnail}
+            size="square"
+            isFeatured={isFeatured}
+          />
+          <div className="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 m-2 rounded-md text-sm font-medium">SALE
+          </div>
+        </div>
+        <div className="p-4">
+          <h3 className="text-lg font-medium mb-2">{productPreview.title}</h3>
+          <p className="text-gray-600 text-sm mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae ante
+            vel eros fermentum faucibus sit amet euismod lorem.</p>
+          <div className="flex items-center justify-between">
+            <span className="font-bold text-lg">
+              {cheapestPrice && <PreviewPrice price={cheapestPrice} />}
+            </span>
+            <Button data-testid="order-details-link" variant="secondary">
+              Buy Now
+            </Button>
           </div>
         </div>
       </div>
